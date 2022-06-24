a46a44c7f10b043ffc381bba97e2d40139a36f37
augustobritodev <augustobritodev@gmail.com>
2022-06-24 17:30:36 -0300

    chore: test env

--- dist/index.js
+++ dist/index.js
@@ -17524,7 +17524,7 @@ async function run() {
     await exec.exec(`zcli apps:validate ${path}`)
     
     console.log(shell.ls(path))
-    console.log(shell.ls('-LA'))
+    console.log(shell.cat('apps/zendesk/dist/zcli.apps.config.json'))
 
     shell.echo(`🚀 Deploying the application...`);
     await exec.exec(`zcli apps:update ${path}`)
