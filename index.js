912598533c41855bbbcdce42f753c03793959838
augustobritodev <augustobritodev@icloud.com>
2022-11-28 15:55:23 -0300

    fix

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ async function run() {
     const environment = core.getInput('ENVIRONMENT');
     const path = core.getInput('PATH');
 
-    if (environment !== 'production' && environment !== 'staging') {
+    if (environment !== 'production' || environment !== 'staging') {
       throw new Error('Environment input must be provided (production and staging).');
     }
 
