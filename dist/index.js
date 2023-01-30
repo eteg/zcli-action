6434bd957ad51a4447ae388c482370e3390788ca
augustobritodev <augustobritodev@icloud.com>
2023-01-30 11:40:05 -0300

    v4

--- dist/index.js
+++ dist/index.js
@@ -11007,9 +11007,9 @@ async function run() {
     await exec.exec('yarn install');
 
     await exec.exec(`echo 🔎 Building, Packaging and Validating...`);
-    await exec.exec(`echo ${process.env} >> .env`);
+    await exec.exec(`echo ${process.env} > .env`);
     await exec.exec(`cat .env`);
-    
+
     await exec.exec(`yarn build`);
     await exec.exec(`yarn zcli apps:package ${path}`);
     
